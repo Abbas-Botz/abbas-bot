cbb3baa88505aa4d04f11c426da6b8c2dbb44243
Abbas-Botz <92032706+Abbas-Botz@users.noreply.github.com>
2021-10-13 23:09:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const { jadibot, stopjadibot, listjadibot } = require('./lib/jadibot')
 
 banChats = true
 offline = false
-targetpc = '6285751056816'
-owner = '6285751056816'
-fake = 'HEXAGONZ'
+targetpc = '628995820020'
+owner = '628995820020'
+fake = 'Abbas'
 numbernye = '0'
 waktu = '-'
 alasan = '-'
